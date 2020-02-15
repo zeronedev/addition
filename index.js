1674aa7cb17c339a1571a3122aaf079c282a910a
ok7375 <ok7375@gmail.com>
2020-02-15 17:27:48 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const question = document.querySelector(".js-question");
+const answer = document.querySelector(".js-answer");
+
+let correctAnswer;
+
+function newQuestion() {
+  const number1 = Math.ceil(Math.random() * 9);
+  const number2 = Math.ceil(Math.random() * 9);
+  correctAnswer = number1 + number2;
+  question.textContent = number1 + " + " + number2 + " = ? ";
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const inputAnswer = Number(input.value);
+  console.log(correctAnswer, inputAnswer);
+  if (inputAnswer === correctAnswer) {
+    answer.textContent = "딩동댕!!! 정답 : " + correctAnswer;
+    input.value = "";
+    newQuestion();
+    input.focus();
+  } else {
+    answer.textContent = "땡!!! 다시 입력하세요";
+    input.value = "";
+    input.focus();
+  }
+}
+
+function init() {
+  newQuestion();
+  input.focus();
+  form.addEventListener("submit", handleSubmit);
+  console.log(question);
+}
+
+init();
